3b10561ecd0412e8b2b6643a3f39dbee2a6ca87e
omer0436 <111450831+omer0436@users.noreply.github.com>
2022-12-01 19:09:41 +0100

    js2-week3-assignment2

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import { fishFarm } from "./fishFarm.js";
+import {
+  stokları500kgUzerinde,
+  frankaraligi9ve12,
+  winterBern,
+  aB10frucuzsiralaAz,
+  totalStok,
+  enPahaliBalik,
+  enUzunDayanabilen,
+  romndykisSonbaharbalikOrtalama,
+  ticinoTotalStock,
+  azSezonuABdisiZHstailanBalikOrtalamasi,
+ } from "./function.js";
+// console.log(stokları500kgUzerinde(fishFarm));
+// console.log(frankaraligi9ve12(fishFarm));
+// console.log(winterBern(fishFarm));
+// console.log(aB10frucuzsiralaAz(fishFarm)); 
+// console.log(totalStok(fishFarm));
+//  console.log(enPahaliBalik(fishFarm));
+//  console.log(enUzunDayanabilen(fishFarm));
+// console.log(romndykisSonbaharbalikOrtalama(fishFarm));
+ console.log(ticinoTotalStock(fishFarm));
+// console.log(azSezonuABdisiZHstailanBalikOrtalamasi(fishFarm));
---
+++ src/function.js
@@ -0,0 +1,100 @@
+//1) Stokları 500 kg'ın üzerinde olan balıkların isimleri nelerdir?
+export const stokları500kgUzerinde = (fishFarm) => {
+  return fishFarm.filter((fish) => fish.stockVolumeInKg > 500);
+};
+
+//2) 9 Fr arasında fiyat aralığına sahip balıklar nelerdir? ve 12 Fr?
+export const frankaraligi9ve12 = (fishFarm) => {
+  return fishFarm.filter((fish) => fish.price >= 9 && fish.price < 12);
+};
+
+//3) Hangi balıklar sadece Bern'de ve sadece kış mevsiminde satılır?
+export const winterBern = (fishFarm) => {
+  return fishFarm.map(
+    (fish) => fish.season === "Winter" && fish.saleLocations.includes === "BE"
+  );
+};
+
+//4) Avrupa Birliği'nden (AB) 10Fr'den daha ucuza mal olan balıkları alfabetik sırayla listeleyin.
+export const aB10frucuzsiralaAz = (fishFarm) => {
+  return fishFarm.filter(
+    (fish) =>
+      (fish.originCountry == "France" ||
+        fish.originCountry == "Poland" ||
+        fish.originCountry == "Italy" ||
+        fish.originCountry == "Spain" ||
+        fish.originCountry == "GREECE") &&
+      fish.price < 10
+  );
+};
+
+// //5) Toplam balık stoğu nedir?
+export const totalStok = (fishFarm) => {
+  return fishFarm
+    .filter((fish) => fish.saleLocations)
+    .map((fish) => fish.stockVolumeInKg)
+    .reduce((a, b) => a + b);
+};
+
+// //6) Balıkları son kullanma tarihlerine göre listeleyin (son kullanma tarihi yaklaşan balıklar önce listelenmelidir).
+// export const sonKullanmatarihi = () => {};
+export const findTheExpirationDate = (fishFarm) => {
+  return fishFarm
+    .filter((fish) => fish.saleLocations)
+    .map((fish) => fish.entryDate)
+    .sort((a, b) => b - a);
+};
+
+// //7) En pahalı balık hangisidir?
+
+export const enPahaliBalik = (fishFarm) => {
+  return fishFarm.reduce((max, min) => (max.price > min.price ? max : min));
+};
+
+// //8) En uzun süre dayanabilen balıklar hangi ülkeden gelmektedir?
+export const enUzunDayanabilen = (pArray) =>
+  pArray
+    .sort((fish1, fish2) => fish2.durationInDays - fish1.durationInDays)
+    .filter((fish) => fish.durationInDays === pArray[0].durationInDays)
+    .map((fish) => fish.originCountry);
+
+// //9) Romandiya'da kış ve sonbahar mevsiminde satılan balıkların ortalama fiyatı nedir?
+const romndykisSonbaharbalikOrtalama = () => {};
+const findTheAveragePriceOfFishSoldInTheRomandyForTheWinterAndFallSeason = (
+  pArray
+) => {
+  // const romandyCantons = ["VD" , "GE" , "NE"] ;
+  const fishPriceArray = pArray
+    .filter(
+      (fish) =>
+        (fish.season === "Winter" || fish.season === "Autumn") &&
+        fish.saleLocations.includes("VD" || "GE" || "NE")
+    )
+    .map((fish) => fish.price);
+};
+return (
+  fishPriceArray.reduce((total, fish) => total + fish) / fishPriceArray.length
+);
+
+// //10) Ticino Kantonu için stokta kaç balık var?
+export const ticinoTotalStock = (fishFarm) =>
+  fishFarm
+    .filter((fish) => fish.saleLocations.includes("TI"))
+    .map((fish) => fish.stockVolumeInKg)
+    .reduce((a, b) => a + b);
+
+// //11) Yaz sezonundan, AB dışından gelen ve ZH'de satılan balıkların ortalama ağırlığını bulun?
+export const yazSezonuABdisiZHstailanBalikOrtalamasi = (pArray) => {
+  const outsideEU = ["Japan", "United Kingdom", "Egypt", "Vietnam"];
+  const fishWeightList = pArray
+    .filter(
+      (fish) =>
+        outsideEU.includes(fish.originCountry) &&
+        fish.saleLocations.includes("ZH") &&
+        fish.season === "Summer"
+    )
+    .map((fish) => fish.itemWeightInGrams);
+};
+return (
+  fishWeightList.reduce((fish, fish1) => fish + fish1) / fishWeightList.length
+);
